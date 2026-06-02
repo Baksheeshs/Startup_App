98e3c3652f5a95a2e0b623fa9bd17ebe5742e566
Baksheeshs <singhbaksheesh7@gmail.com>
2026-06-02 18:18:06 +0530

    Initial Commit

---
+++ Stratup_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stratup_App
+//
+//  Created by Baksheesh Singh on 02/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stratup_App/Stratup_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stratup_AppApp.swift
+//  Stratup_App
+//
+//  Created by Baksheesh Singh on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Stratup_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
